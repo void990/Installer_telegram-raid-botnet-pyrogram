f917c792c3079ba0cfdb464ed7c98eb2ed8bc54c
void990 <110184248+void990@users.noreply.github.com>
2022-08-03 21:05:15 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ if exactly == "y":
 
    console.print("[bold green][ ~ ] [white]Running...\n")
    os.system("pip3 install phonenumbers\n")
-   os.system("cd telegram-raid-botnet-pyrogram && pip3 install -r requirements.txt && python3 main.py\n")
+   os.system("cd telegram-raid-botnet-pyrogram && pip3 install -r requirements.txt && cat README.md && python3 main.py\n")
    console.print("[bold green][ + ] [white]Installed\n")
 
 
